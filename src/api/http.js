61e23c8f544bbf3faa3d0e44e4cc6b8b7e3bf721
Menaka2024 <murukaiya.menaka@gmail.com>
2026-06-06 10:36:22 +0530

    my first commit

--- src/api/http.js
+++
@@ -1,8 +0,0 @@
-import axios from "axios";
-
-// Backend base URL change pannunga:
-// ex: http://localhost:8000
-export const http = axios.create({
-  baseURL: import.meta.env.VITE_API_BASE_URL || "http://localhost:8000",
-  timeout: 20000,
-});
